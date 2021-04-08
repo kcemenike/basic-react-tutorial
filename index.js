2e830d95f529feb6b8ecb4e7bfad4153cd1874d6
KC <noreply@kcemenike.github.com>
2021-04-08 12:51:06 +0100

    use JSX (with Babel transpiler) instead of pure React JS

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 // Component render
 class App extends React.Component {
   render() {
-    return React.createElement('div', { id: 'name' }, 'My name is KC')
-    /**
-    'div' => type of element, in this case, div
-    { id: 'name' } =>// properties that children will have. Can also be null
-    'My name is Kelechi' => the child(ren)
-    )
-     */
+    return <div id='name'>My name is KC</div>
+    // return React.createElement('div', { id: 'name' }, 'My name is KC')
   }
 }
 
 // Render App to DOM
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
