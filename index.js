de4c17ec82d52eee38f07288292118520c30e063
KC <noreply@kcemenike.github.com>
2021-04-08 13:03:17 +0100

    add individual components to app

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-// Component render
+// Create components
+
+/// Parent component (it's actually a JS function)
+function Parent() {
+  return (
+    <div>
+      <h1>The Parent</h1>
+      <h3>Name: </h3>
+    </div>
+  )
+}
+
+/// Child component (let's use arrow function this time)
+const Child = () => {
+  return (
+    <div>
+      <h1>The Child</h1>
+      <h3>Name: </h3>
+    </div>
+  )
+}
+
+// Render components to App
 class App extends React.Component {
   render() {
-    return <div id='name'>My name is KC</div>
-    // return React.createElement('div', { id: 'name' }, 'My name is KC')
+    return <div>
+      <Parent />
+      <Child />
+    </div>
   }
 }
 
